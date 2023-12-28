f20a82d32ac29aaa3dc9d3d4f260ae14a4544bef
sirius1y <sirius1y@outlook.com>
2023-12-28 14:19:52 +0800

    feat: 完成排行榜对于所有连接的ws进行广播，修复ranking使用map的时候会出现乱序的问题，更改为使用切片

--- backend/controllers/websocket.go
+++ backend/controllers/websocket.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"scoreboard/services"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -16,6 +17,10 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+// 记录所有打开的 WebSocket 连接
+var connections []*websocket.Conn
+
+// 判断字符串是否为 JSON 格式
 func isJSON(str string) bool {
 	var js json.RawMessage
 	return json.Unmarshal([]byte(str), &js) == nil
@@ -32,8 +37,23 @@ func HandleWebSocket(c *gin.Context) {
 		log.Printf("failed to upgrade GET request: %v", err)
 		return
 	}
+
+	connections = append(connections, ws)
+
 	// 使用 defer 语句确保 WebSocket 连接在函数返回时关闭
-	defer ws.Close()
+	defer func(ws *websocket.Conn) {
+		err := ws.Close()
+		if err != nil {
+
+		}
+		// remove the connection from the list
+		for i, conn := range connections {
+			if conn == ws {
+				connections = append(connections[:i], connections[i+1:]...)
+				break
+			}
+		}
+	}(ws)
 
 	// 进入一个无限循环，持续监听 WebSocket 连接上的消息
 	for {
@@ -56,7 +76,7 @@ func HandleWebSocket(c *gin.Context) {
 
 		// 检查message是否为json格式
 		if !isJSON(string(message)) {
-			log.Printf("message is not json: %v", message)
+			log.Printf("WARNING: The essage is not json: %v", message)
 			break
 		}
 
@@ -71,7 +91,7 @@ func HandleWebSocket(c *gin.Context) {
 		// 从解析后的消息中获取 "type" 字段的值，并将其转换为 string 类型
 		// 这里假设 "type" 字段的值是一个字符串，表示消息的类型
 		messageType := msg["type"]
-		log.Printf(messageType)
+		log.Printf("Received a new message, and the type is " + msg["type"])
 
 		// 使用 switch 语句根据消息类型进行不同的处理
 		switch messageType {
@@ -80,18 +100,59 @@ func HandleWebSocket(c *gin.Context) {
 			// 从解析后的消息中获取 "nickname" 字段的值，并将其转换为 string 类型
 			// 然后调用 services.AddnewUser 函数添加新用户
 			services.AddNewUser(msg["nickname"])
+			log.Printf("New user added: " + msg["nickname"])
 
 		case "UserClick":
 			// 当消息类型为 "UserClick" 时，处理用户点击逻辑
 			// 从解析后的消息中获取 "nickname" 字段的值，并将其转换为 string 类型
 			// 然后调用 services.HandleUserClick 函数处理用户点击
 			services.HandleUserClick(msg["nickname"])
+			log.Printf("User " + msg["nickname"] + " clicked")
 
 		case "UserInactive":
 			// 当消息类型为 "UserInactive" 时，处理用户不活跃逻辑
 			// 从解析后的消息中获取 "nickname" 字段的值，并将其转换为 string 类型
 			// 然后调用 services.HandleUserInactive 函数处理用户不活跃
-			services.HandleUserInactive(msg["nickname"])
+			err = services.HandleUserInactive(msg["nickname"])
+			if err == nil {
+				log.Printf("User inactive: " + msg["nickname"])
+			}
 		}
 	}
+}
+
+func init() {
+	// 使用 go 关键字启动一个新的 goroutine
+	go func() {
+		// 使用无限循环，使得这个 goroutine 会一直运行
+		for {
+			// 每隔一分钟执行一次循环体中的代码
+			time.Sleep(200 * time.Millisecond) // 可根据需要调整时间间隔
+
+			// 调用 services.GetRanking() 函数获取用户排名
+			ranking, err := services.GetRanking()
+			// 如果在获取用户排名时发生错误，打印错误信息并跳过本次循环的剩余部分
+			if err != nil {
+				log.Printf("error getting ranking: %v", err)
+				continue
+			}
+
+			// 使用 json.Marshal 函数将用户排名转换为 JSON 格式
+			message, err := json.Marshal(ranking)
+			// 如果在转换为 JSON 格式时发生错误，打印错误信息并跳过本次循环的剩余部分
+			if err != nil {
+				log.Printf("error marshalling ranking: %v", err)
+				continue
+			}
+
+			// 遍历所有打开的 WebSocket 连接
+			for _, conn := range connections {
+				// 使用 conn.WriteMessage 方法向每个连接发送包含用户排名的消息
+				// 如果在发送消息时发生错误，打印错误信息
+				if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+					log.Printf("error writing message: %v", err)
+				}
+			}
+		}
+	}()
 }
--- backend/services/redis-server.go
+++ backend/services/redis-server.go
@@ -3,9 +3,8 @@ package services
 // 导入需要的包，包括 Redis 客户端库、日志和格式化输出
 import (
 	"fmt"
-	"log"
-
 	"github.com/go-redis/redis"
+	"log"
 )
 
 // 创建一个 Redis 客户端，连接到本地的 Redis 服务器
@@ -23,8 +22,8 @@ func init() {
 		// 创建一个 sorted set，用于存储用户的点击次数
 		// 这里先添加了一个初始用户 "user:1"，点击次数为 0
 		client.ZAdd("users", redis.Z{
-			Score:  0,        // 用户的点击次数
-			Member: "user:1", // 用户的 ID
+			Score:  10,        // 用户的点击次数
+			Member: "default", // 用户的 ID
 		})
 	} else {
 		// 如果已经存在，就清空它
@@ -44,27 +43,43 @@ func AddNewUser(userID string) {
 // 处理用户点击的函数，当用户点击时，增加其在 sorted set 中的分数（即点击次数）
 func HandleUserClick(userID string) {
 	// 增加用户的点击次数
-	client.ZIncrBy("userClicks", 1, userID)
+	client.ZIncrBy("users", 1, userID)
 }
 
 // 获取用户点击排行榜的函数，打印出点击次数最多的前 10 个用户
-func GetRanking() {
-	// 获取点击次数最多的前 10 个用户
-	ranking, err := client.ZRevRangeWithScores("userClicks", 0, 9).Result()
-	// 如果获取排行榜时出错，打印错误信息并返回
+// 定义一个结构体来存储用户ID和点击次数
+type UserScore struct {
+	ID    string
+	Score float64
+}
+
+func GetRanking() ([]UserScore, error) {
+	// 从Redis的sorted set中获取点击次数最多的前10个用户
+	ranking, err := client.ZRevRangeWithScores("users", 0, 9).Result()
 	if err != nil {
-		log.Printf("error getting ranking: %v", err)
-		return
+		return nil, fmt.Errorf("error getting ranking: %v", err)
 	}
 
-	// 打印排行榜
+	// 创建一个切片来存储用户ID和点击次数
+	result := make([]UserScore, len(ranking))
 	for i, user := range ranking {
-		fmt.Printf("%d: %s (%f clicks)\n", i+1, user.Member, user.Score)
+		// 将用户ID和点击次数存入切片
+		result[i] = UserScore{ID: user.Member.(string), Score: user.Score}
 	}
+
+	// 返回切片，切片中的元素顺序与它们在Redis中的顺序相同
+	return result, nil
 }
 
 // 处理不活跃用户的函数，当用户不活跃时，将其从 sorted set 中删除
-func HandleUserInactive(userID string) {
+func HandleUserInactive(userID string) (err error) {
+	_, err = client.ZScore("users", userID).Result()
+	if err != nil {
+		log.Printf("the user " + userID + " don't exist")
+		// 如果用户不存在，就直接返回
+		return err
+	}
 	// 从 sorted set 中删除用户
 	client.ZRem("users", userID)
+	return err
 }
