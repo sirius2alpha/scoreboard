7178e0f4535f5e836e1a7cded6e2323a8f360d31
sirius1y <sirius1y@outlook.com>
2024-04-08 10:27:35 +0800

    chore: Adjust the project directory structure

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-cd ../src/frontend/
-npm run build
-scp -r dist root@sirius1y.top:/var/www/scoreboard/frontend/
-
-cd ../src/backend/
-go build -o main
-scp main root@sirius1y.top:/var/www/scoreboard/backend/
