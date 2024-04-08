43fffd029d784bb404be3e73569905267e5689d4
sirius1y <sirius1y@outlook.com>
2024-04-08 10:24:37 +0800

    chore: Adjust the project directory structure

---
+++ app/frontend/src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css';
+
+import {createApp} from 'vue';
+import {createPinia} from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import {initWebsocket} from '@/utils/websocketInstance.js';
+
+initWebsocket();
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.use(ElementPlus);
+
+app.mount('#app');
---
+++ app/frontend/src/utils/websocketInstance.js
@@ -0,0 +1,25 @@
+export const initWebsocket = () => {
+    // const ws = new WebSocket('ws://1.94.126.139:8080/ws');
+    const ws = new WebSocket('ws://127.0.0.1:8001/ws');
+    ws.onopen = () => {
+        console.log('connected');
+    };
+    ws.onMessage = (callback) => {
+       ws.onmessage = (msg) => callback(JSON.parse(msg.data));
+    }
+    ws.sendMessage = (msg) => {
+        ws.send(JSON.stringify(msg));
+    }
+    ws.onclose = () => {
+        console.log('disconnected');
+    };
+    ws.onerror = (err) => {
+        console.error(
+            'Socket encountered error: ',
+            err.message,
+            'Closing socket'
+        );
+        ws.close();
+    };
+    window.$ws = ws;
+}
